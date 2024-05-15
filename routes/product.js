592ae7b578b7da7dffae3debdb82aa4c3a172339
norbeyho <norbeyho@hotmail.com>
2024-05-14 22:50:59 -0500

    Se agrega buscar producto por id

--- routes/product.js
+++ routes/product.js
@@ -14,13 +14,21 @@ router.post('/products', cors(),(req, res)=>{
         .save()
         .then((data) => res.json(data))
         .catch((error)=> res.json({message: error}))
-})
+});
 
 router.get('/products', cors(),(req, res)=>{
     productSchema
         .find()
         .then((data)=> res.json(data))
         .catch((error)=> res.json({message: error}))
-})
+});
+
+router.get('/products/:categoryId', cors(), (req, res) => {
+    const {categoryId} = req.params; 
+    productSchema
+        .findOne({ categoryId:categoryId }) 
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
 
 module.exports = router;
