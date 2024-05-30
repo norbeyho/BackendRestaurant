1ac56368e44c0ddca1f138e65b9a8beff779d90f
norbeyho <norbeyho@hotmail.com>
2024-05-29 21:29:19 -0500

    se actualiza employees

--- routes/order.js
+++ routes/order.js
@@ -24,14 +24,20 @@ router.get('/orders', cors(), (req, res)=>{
         .catch((error)=> res.json({message: error}))
 });
 
-// Recuperar orden por id
- router.get('/orders/:id', cors(), (req, res)=>{
-    const { id } = req.params;
-    orderSchema
-        .findById(id)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}))
- });
+// Recuperar orden por mesa y estado
+router.get('/orders', async (req, res) => {
+    const { tableName, progress } = req.query;
+
+    try {
+        const order = await orderSchema.findOne({ tableName, progress });
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
 
 // Actualizar una orden
 router.put('/orders/:id', cors(), (req, res) => {
